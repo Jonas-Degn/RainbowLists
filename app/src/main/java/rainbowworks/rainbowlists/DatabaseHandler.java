810b03a432e32db54f9ebef653189816de24aef2
Dimitar <dimixxx69@gmail.com>
2015-04-07 12:03:42 +0200

    Created skeleton class

---
+++ app/src/main/java/rainbowworks/rainbowlists/DatabaseHandler.java
@@ -0,0 +1,29 @@
+package rainbowworks.rainbowlists;
+
+/**
+ * Created by Dimitar on 7.4.2015 г..
+ */
+public class DatabaseHandler {
+
+    private static String dbName;
+
+    public DatabaseHandler(String name) {
+
+    }
+
+    public static void connect() {
+
+    }
+
+    public static void create() {
+
+    }
+
+    public static void save(String sql) {
+
+    }
+
+    public static String[] load(String sql) {
+
+    }
+}
