a59c4d7475f21b8cc46219a40dfbee5eefee54b4
Dimitar <dimixxx69@gmail.com>
2015-04-07 12:27:27 +0200

    Created skeleton class

--- app/src/main/java/rainbowworks/rainbowlists/DatabaseHandler.java
+++ app/src/main/java/rainbowworks/rainbowlists/DatabaseHandler.java
@@ -1,29 +1,32 @@
 package rainbowworks.rainbowlists;
 
+import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
+import static android.content.Context.*;
+
 /**
  * Created by Dimitar on 7.4.2015 г..
  */
-public class DatabaseHandler {
+public class DatabaseHandler extends Activity{
 
-    private static String dbName;
+    private SQLiteDatabase db;
 
-    public DatabaseHandler(String name) {
+    protected DatabaseHandler(String name, MainActivity activity) {
+        db = SQLiteDatabase.openOrCreateDatabase(name, activity.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS");
+    }
+
+    protected static void connect() {
 
     }
 
-    public static void connect() {
+    protected static void save(String sql) {
 
     }
 
-    public static void create() {
-
-    }
-
-    public static void save(String sql) {
-
-    }
-
-    public static String[] load(String sql) {
+    protected static String[] load(String sql) {
 
     }
 }
