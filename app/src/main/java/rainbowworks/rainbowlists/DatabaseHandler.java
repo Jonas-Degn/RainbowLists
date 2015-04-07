d66bbb9de3eb5ed7c9a4cf2189ccdf7170813aed
Dimitar <dimixxx69@gmail.com>
2015-04-07 13:52:38 +0200

    First implementation of DatabaseHandler with ready methods.

--- app/src/main/java/rainbowworks/rainbowlists/DatabaseHandler.java
+++ app/src/main/java/rainbowworks/rainbowlists/DatabaseHandler.java
@@ -1,32 +1,38 @@
 package rainbowworks.rainbowlists;
 
-import android.app.Activity;
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
-import static android.content.Context.*;
+import java.util.ArrayList;
 
 /**
  * Created by Dimitar on 7.4.2015 г..
  */
-public class DatabaseHandler extends Activity{
+public class DatabaseHandler{
 
     private SQLiteDatabase db;
 
-    protected DatabaseHandler(String name, MainActivity activity) {
-        db = SQLiteDatabase.openOrCreateDatabase(name, activity.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS");
+    protected DatabaseHandler(MainActivity activity) {
+        db = activity.openOrCreateDatabase("rainbowData", activity.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS items (ID int(8) AUTO_INCREMENT, listID int(8), name VARCHAR(32), PRIMARY KEY (ID));");
+        db.execSQL("CREATE TABLE IF NOT EXISTS lists (ID int(8) AUTO_INCREMENT, name VARCHAR(32), amount int, PRIMARY KEY (ID));");
     }
 
-    protected static void connect() {
-
+    //The sql string contains the command to be passed to execSQL();
+    protected void save(String sql) {
+        db.execSQL(sql);
     }
 
-    protected static void save(String sql) {
+    protected ArrayList[][] load(String sql) {
+        Cursor c=db.rawQuery(sql, null);
+        //2-dim Arraylist. getCount() returns ROWS COUNT
+        ArrayList[][] stringArray = new ArrayList[c.getCount()][c.getColumnCount()];
 
-    }
+        for (int i=0; c.moveToNext(); i++){
+            for (int k = 0; k < c.getColumnCount(); k++) {
+                stringArray[i][k].add(c.getString(k));
+            }
+        }
 
-    protected static String[] load(String sql) {
-
+        return stringArray;
     }
 }
