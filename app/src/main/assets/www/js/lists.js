8c995970dae992063015ac82005b143651ff2133
Jonas Degn <jonas@jdu.dk>
2015-04-11 15:26:37 +0200

    minor mistake in showing lists.

--- app/src/main/assets/www/js/lists.js
+++ app/src/main/assets/www/js/lists.js
@@ -10,12 +10,12 @@ function loadLists() {
         var dataFields = data.split(",");
         var id = dataFields[0];
         var name = dataFields[1];
-        $shopping.append(' <div id="'+id+'" class="list_block">'+name+' <img class="threeDots" src="images/3_dots_list.png"><img class="listLine" src="images/list_line.png"></div>');
+        $shopping.after('<div id="'+id+'" class="list_block">'+name+' <img class="threeDots" src="images/3_dots_list.png"><img class="listLine" src="images/list_line.png"></div>');
     }
     for(i = 0; i < pantryData.length-1; i++) {
         var dataFields = data.split(",");
         var id = dataFields[0];
         var name = dataFields[1];
-        $pantry.append(' <div id="'+id+'" class="list_block">'+name+' <img class="threeDots" src="images/3_dots_list.png"><img class="listLine" src="images/list_line.png"></div>');
+        $pantry.after('<div id="'+id+'" class="list_block">'+name+' <img class="threeDots" src="images/3_dots_list.png"><img class="listLine" src="images/list_line.png"></div>');
     }
 }
