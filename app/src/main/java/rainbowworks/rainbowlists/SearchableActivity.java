acfca2d2bb4e90ea4b182b636d5cab4d31d4089f
Jonas Degn <jonas@jdu.dk>
2015-04-12 18:12:09 +0200

    Push to dimitar

--- app/src/main/java/rainbowworks/rainbowlists/SearchableActivity.java
+++ app/src/main/java/rainbowworks/rainbowlists/SearchableActivity.java
@@ -6,30 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.List;
+
 /**
  * Created by Dimitar on 12.4.2015 г..
  * SearchableActivity extends ListView as that is how it is done traditionally.
  */
 public class SearchableActivity extends ListView {
     Intent intent;
+    MainActivity activity;
 
-    public SearchableActivity(Context context) {
+    public SearchableActivity(Context context, MainActivity activity) {
         super(context);
+        this.activity = activity;
     }
 
-    public String getQuery() {
+    public List<List<String>> getQuery() {
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         String query = null;
+        List<List<String>> result = null;
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             query = intent.getStringExtra(SearchManager.QUERY);
-            /*
-            *   or we could pass the query to another method to do sth with it
-            *   doMySearch(query);
-            */
+            result = activity.getDBH().load("SELECT * FROM lists WHERE name LIKE '%"+query+"%'");
         }
-        return query;
+        return result;
     }
 
     public Intent getIntent() {
