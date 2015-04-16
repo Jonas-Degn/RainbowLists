dc537aa67bbd8135ae71ad9ac894a65f18f0ab93
Dimitar <dimixxx69@gmail.com>
2015-04-16 15:20:32 +0200

    Broken code to be fixed

---
+++ app/src/main/java/rainbowworks/rainbowlists/DrawerItemClickListener.java
@@ -0,0 +1,39 @@
+package rainbowworks.rainbowlists;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+/**
+ * Created by Dimitar on 16.4.2015 г..
+ */
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+    public DrawerItemClickListener() {
+        Log.i("constructor", "test if works");
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("onItemClick method", "test if works");
+        selectItem(position);
+    }
+
+    public void selectItem(int position) {
+
+        Log.i("Drawer", Integer.toString(position));
+        switch (position) {
+            case 0:
+                JavaInterface.runJS("loadPage('settings.html')");
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+}
